68085356aaf54897f864d5464092637d62e2c19c
Alan Vardy <alanvardy@gmail.com>
2018-10-23 16:18:07 -0600

    add tests

--- hartl-sample_app/app/controllers/users_controller.rb
+++ hartl-sample_app/app/controllers/users_controller.rb
@@ -1,9 +1,31 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
